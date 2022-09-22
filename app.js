7890d8511aee2ed924cde2d710f40c1907da074f
aldanamiranda <aldanamiranda@yahoo.com.ar>
2022-09-22 20:39:31 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const path= require('path');
+
+const app = express();
+
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+app.listen(process.env.PORT || 3000, ()=>{
+    console.log ('servidor corriendo en puerto 3000')
+})
+app.use(express.urlencoded({ extended: false }))
+
+app.get ('/', (req,res)=>{
+    res.sendFile(path.resolve(__dirname, 'src/views/home.html'))
+})
+app.get ('/register', (req,res)=>{
+    res.sendFile(path.resolve(__dirname, 'src/views/register.html'))
+})
+
+app.get ('/login', (req,res)=>{
+    res.sendFile(path.resolve(__dirname, 'src/views/login.html'))
+})
